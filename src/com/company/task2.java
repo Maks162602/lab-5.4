bc7bce20c7ba72390ad4e70b6b453406f9a45384
Maks162602 <100307397+Maks162602@users.noreply.github.com>
2022-05-12 10:42:56 +0300

    Add files via upload

---
+++ src/com/company/task2.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class task2 {
+
+        public static void main(String[] args) {
+            System.out.println("Батько: Миклуш Андрій Олексійович, 1969, Директор ЖКО");
+            System.out.println("Мама : Миклуш Наталя Теодорівна, 1972, Начальник дільниці ЖКО");
+            System.out.println("Сестра : Середа Адріана Андріївна, 1992, ЛДУ БЖД фахівець РТС");
+            System.out.println("Сестра : Миклуш Руслана Андріївна, 1996, ГУ ДСНС 59 ДПРП з охорони об'єктів");
+            System.out.println("Сестра : Миклшу Єва Андріївна , 2017, ДНЗ");
+
+        }
+}
